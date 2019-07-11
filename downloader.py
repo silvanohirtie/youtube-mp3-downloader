f69463e6b7e25d43511c17487bda43e5cc5671bd
Silvano <40663430+MyNameIsMental@users.noreply.github.com>
2019-07-11 18:29:39 +0200

    create downloader.py

---
+++ downloader.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+import youtube_dl
+print("Insert the link")
+link = input ("")
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([link])
+
+print("Contact me on discord for any problem: https://discord.gg/BgjwgWx")
